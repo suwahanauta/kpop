176dd6c5c7caa5e72a56114ba09c158910b8d3c3
suwahanauta <suwa.hnut@gmail.com>
2024-02-19 14:14:41 +0900

    グループボタンの詳細決定

--- app/groups/create/page.tsx
+++ app/groups/create/page.tsx
@@ -3,8 +3,18 @@ import Image from "next/image";
 export default function Home() {
   return (
 
-    <div>
-        
+    <div className="">
+        <div>
+          <p>グループ画像</p>
+
+        </div>
+        <div>
+          <p>グループ名</p>
+          <input></input>
+        </div>
+        <div>
+          <button className="bg-pink-300 font-bold h-[calc(100vh/10)] w-[calc(100vw/4)] rounded-full absolute bottom-[100px] left-[calc(100vw/8*3)]">グループ作成</button>
+        </div>
     </div>
 
   );
