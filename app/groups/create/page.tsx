7da3dda1d6c207550dc9b68bf758cbf910be7358
suwahanauta <suwa.hnut@gmail.com>
2024-02-21 10:09:52 +0900

    useStateからuseRefを用いた実装に変更

--- app/groups/create/page.tsx
+++ app/groups/create/page.tsx
@@ -6,28 +6,27 @@ import { useRef } from "react";
 
 export default function Home() {
 
-    const ref1 = useRef("");
+    const textRef = useRef<HTMLInputElement>(null);
 
-    const ref2 = useRef("");
+    const imageRef = useRef<HTMLInputElement>(null);
 
     return (
         <div>
 
             <div className="h-[100px] mt-[50px]">
                 <p className="text-center font-bold text-[30px] leading-[100px]">↓グループ名を入力↓</p>
-                <input onChange={(e) => {ref1.current = e.target.value} } className="h-[50px] w-[calc(100vw/4)] rounded-lg border-[3px] border-pink-300 absolute top-[150px] left-[calc(100vw/8*3)]"></input>
+                 <input ref={textRef} className="h-[50px] w-[calc(100vw/4)] rounded-lg border-[3px] border-pink-300 absolute top-[150px] left-[calc(100vw/8*3)]"></input>
             </div>
 
             <div className="h-[100px] mt-[100px]">
                 <p className="text-center font-bold text-[30px] leading-[100px]">↓グループ画像を選択↓</p>
-                <input onChange={(e) => {ref2.current = e.target.value} } className="bg-white text-[20px] rounded-lg p-[10px] border-[3px] border-pink-300 w-[calc(100vw/4)] absolute top-[350px] left-[calc(100vw/8*3)]" type="file"></input>
+                <input ref={imageRef} className="bg-white text-[20px] rounded-lg p-[10px] border-[3px] border-pink-300 w-[calc(100vw/4)] absolute top-[350px] left-[calc(100vw/8*3)]" type="file"></input>
             </div>
 
             <div>
-                <button onClick={() => {console.log({ref1, ref2})}} className="bg-pink-300 font-bold h-[calc(100vh/10)] w-[calc(100vw/4)] rounded-full absolute bottom-[100px] left-[calc(100vw/8*3)] text-white text-[20px]">グループ作成</button>
+                <button onClick={() => {console.log(textRef.current?.value, imageRef.current?.value)}} className="bg-pink-300 font-bold h-[calc(100vh/10)] w-[calc(100vw/4)] rounded-full absolute bottom-[100px] left-[calc(100vw/8*3)] text-white text-[20px]">グループ作成</button>
             </div>
 
         </div>
     );
 }
-
