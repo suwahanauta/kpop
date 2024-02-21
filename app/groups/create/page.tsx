eddbe99d713f99f7d5080fe1a62859b187139405
suwahanauta <suwa.hnut@gmail.com>
2024-02-21 10:10:55 +0900

    インデントを修正

--- app/groups/create/page.tsx
+++ app/groups/create/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 
             <div className="h-[100px] mt-[50px]">
                 <p className="text-center font-bold text-[30px] leading-[100px]">↓グループ名を入力↓</p>
-                 <input ref={textRef} className="h-[50px] w-[calc(100vw/4)] rounded-lg border-[3px] border-pink-300 absolute top-[150px] left-[calc(100vw/8*3)]"></input>
+                <input ref={textRef} className="h-[50px] w-[calc(100vw/4)] rounded-lg border-[3px] border-pink-300 absolute top-[150px] left-[calc(100vw/8*3)]"></input>
             </div>
 
             <div className="h-[100px] mt-[100px]">
@@ -24,7 +24,7 @@ export default function Home() {
             </div>
 
             <div>
-                <button onClick={() => {console.log(textRef.current?.value, imageRef.current?.value)}} className="bg-pink-300 font-bold h-[calc(100vh/10)] w-[calc(100vw/4)] rounded-full absolute bottom-[100px] left-[calc(100vw/8*3)] text-white text-[20px]">グループ作成</button>
+                <button onClick={() => { console.log(textRef.current?.value, imageRef.current?.value) }} className="bg-pink-300 font-bold h-[calc(100vh/10)] w-[calc(100vw/4)] rounded-full absolute bottom-[100px] left-[calc(100vw/8*3)] text-white text-[20px]">グループ作成</button>
             </div>
 
         </div>
