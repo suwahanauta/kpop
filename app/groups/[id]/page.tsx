15e044872df4f996fe03d792a4fca0c6abee747a
suwahanauta <suwa.hnut@gmail.com>
2024-03-04 14:15:51 +0900

    セグメントを動的にして、各画像の詳細画面を作成

---
+++ app/groups/[id]/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import Image from "next/image";
+
+export default function Home() {
+
+    return (
+        <div className="h-[calc(100vh/10)] w-screen">
+        <h1 className="font-bold text-[30px] text-center leading-[calc(100vh/10)]">Kpopグループ詳細</h1>
+      </div>
+    );
+}
--- app/groups/page.tsx
+++ app/groups/page.tsx
@@ -13,7 +13,6 @@ type GroupInfo = {
 
 export default function Home() {
 
-
   let [state, setState] = useState<GroupInfo[]>([])
 
   useEffect(() => {
@@ -40,7 +39,11 @@ export default function Home() {
         {state.map(
           (value, index) => {
             return <div key={index} className="h-[calc(100vw/4)] w-[calc(100vw/4)] p-[30px]">
-              <img onClick={ () => { console.log("⭐")}} className="h-4/5 w-full" src={value.image_url} />
+              <img onClick={ () => { 
+
+                window.location.href = `/groups/${index+1}`
+
+              }} className="h-4/5 w-full" src={value.image_url} />
               <p className="h-1/5 w-full text-center text-[20px] leading-{h-5/1}">{value.name}</p>
             </div>
           }
